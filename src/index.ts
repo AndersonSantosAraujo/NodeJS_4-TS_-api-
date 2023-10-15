f91d984101da477f18f71df00a662fb1313bde2e
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-10-15 17:30:33 -0300

    feat: add CreateUserController with validations

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,40 @@ import "dotenv/config";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoClient } from "./database/mongo";
+import { MongoCreateUserRepository } from "./repositories/create-user/mongo-create-users";
+import { CreateUserController } from "./controllers/create-user/create-users";
 
 const main = async () => {
   const app = express();
+  app.use(express.json());
 
   await MongoClient.connect();
 
+  // Route 1
   app.get("/users", async (req, res) => {
     const mongoGetUsersRepository = new MongoGetUsersRepository();
     const getUsersController = new GetUsersController(mongoGetUsersRepository);
 
     const { body, statusCode } = await getUsersController.handle();
 
-    res.send(body).status(statusCode);
+    res.status(statusCode).send(body);
   });
 
+  // Route 2
+  app.post("/users", async (req, res) => {
+    const mongoCreateUserRepository = new MongoCreateUserRepository();
+    const createUserController = new CreateUserController(
+      mongoCreateUserRepository,
+    );
+
+    const { body, statusCode } = await createUserController.handle({
+      body: req.body,
+    });
+
+    res.status(statusCode).send(body);
+  });
+
+  // Server Listening
   const port = process.env.PORT || 8000;
 
   app.listen(port, () => {
