2221365ed9f6c0cc7447a99a2b8d9489c82f7506
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-10-15 02:57:01 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("1234");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Listening on por ${port}...`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
