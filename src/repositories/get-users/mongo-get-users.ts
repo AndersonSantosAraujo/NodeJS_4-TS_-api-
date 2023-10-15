d41cab97f75339a7b6f0d98f5572a90e5843cb7a
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-10-15 16:14:56 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,17 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "Anderson",
-        lastName: "Araujo",
-        email: "anderson@araujo.com",
-        password: "123",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
