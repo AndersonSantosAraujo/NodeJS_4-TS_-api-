5b3f2374cfa22c91ed3a236abb5cdaa4898b8e68
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-10-15 23:53:01 -0300

    feat: add MongoUser protocol

--- src/repositories/update-user/mongo-update-user.ts
+++ src/repositories/update-user/mongo-update-user.ts
@@ -5,6 +5,7 @@ import {
 } from "../../controllers/update-user/protocols";
 import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
+import { MongoUser } from "../mongo-protocols";
 
 export class MongoUpdateUserRepository implements IUpdateUserRepository {
   async updateUser(id: string, params: UpdateUserParams): Promise<User> {
@@ -18,7 +19,7 @@ export class MongoUpdateUserRepository implements IUpdateUserRepository {
     );
 
     const user = await MongoClient.db
-      .collection<Omit<User, "id">>("users")
+      .collection<MongoUser>("users")
       .findOne({ _id: new ObjectId(id) });
 
     console.log("TETE ", user);
