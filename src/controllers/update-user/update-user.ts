a444a48d81fc6ea257ba1d9ba82a2a34577dd644
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-10-15 23:26:26 -0300

    refactor: add IController common interface

--- src/controllers/update-user/update-user.ts
+++ src/controllers/update-user/update-user.ts
@@ -1,18 +1,23 @@
 import { User } from "../../models/user";
-import { HttpRequest, HttpResponse } from "../protocols";
-import {
-  IUpdateUserController,
-  IUpdateUserRepository,
-  UpdateUserParams,
-} from "./protocols";
+import { HttpRequest, HttpResponse, IController } from "../protocols";
+import { IUpdateUserRepository, UpdateUserParams } from "./protocols";
 
-export class UpdateUserController implements IUpdateUserController {
+export class UpdateUserController implements IController {
   constructor(private readonly updateUserRepository: IUpdateUserRepository) {}
 
-  async handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<User>> {
+  async handle(
+    httpRequest: HttpRequest<UpdateUserParams>,
+  ): Promise<HttpResponse<User>> {
     try {
       const id = httpRequest?.params?.id;
       const body = httpRequest?.body;
+
+      if (!body) {
+        return {
+          statusCode: 400,
+          body: "Missing fields.",
+        };
+      }
 
       if (!id) {
         return {
@@ -27,7 +32,7 @@ export class UpdateUserController implements IUpdateUserController {
         "password",
       ];
 
-      const someFieldNotAllowedToUpdate = Object.keys(body).some(
+      const someFieldNotAllowedToUpdate = Object.keys(body!).some(
         (key) => !allowedFieldToUpdate.includes(key as keyof UpdateUserParams),
       );
 
